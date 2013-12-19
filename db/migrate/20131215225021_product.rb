697fbd780c303f7be8fb299cc43cd8660f8aa274
JasonLev <jasond.levy@gmail.com>
2013-12-19 09:38:46 -0800

    edit for pg migration; modal fix

--- db/migrate/20131215225021_product.rb
+++ db/migrate/20131215225021_product.rb
@@ -1,7 +1,6 @@
 class Product < ActiveRecord::Migration
   def change
     remove_column :products, :votes
-    remove_column :products, :guidance_id, :integer
     add_column :products, :image_url
   end
 end
